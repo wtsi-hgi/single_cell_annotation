e63e663b39b1331e0d7efc1bb69de31118e4bfde
Guillaume G Noell <gn5@sanger.ac.uk>
2022-02-22 16:53:35 +0000

    credits

--- bin/sync_results_to_gitlab.sh
+++ bin/sync_results_to_gitlab.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
+set -e
+RESULTS_DIR=$1
+cd $RESULTS_DIR
 
-
-
+# source private gitlab token to clone repo, see instructions below
 source /nfs/users/nfs_m/mercury/secrets/gitlab_scrna_cellranger_tokens.sh
-git clone https://${CI_DEPLOY_USER}:${CI_DEPLOY_TOKEN}@${REPO_HTTPS} ${REPO_DIR} > /dev/null 2>&1
+export REPO_HTTPS=gitlab.internal.sanger.ac.uk/hgi-projects/scrna_cellranger.git
+export REPO_DIR=scrna_cellranger
+if [ ! -d "${REPO_DIR}" ]; then
+  git clone https://${CI_DEPLOY_USER}:${CI_DEPLOY_TOKEN}@${REPO_HTTPS} ${REPO_DIR} > /dev/null 2>&1
+fi
 
-
+cd ${REPO_DIR}
+git pull
+mkdir -p ./sync_status/keras_celltypes
+cp ../*.csv ./sync_status/keras_celltypes/
+git add ./sync_status/keras_celltypes/*.csv
+git commit -m "keras pipeline run complete"
+git push
 
 
 ## uses secret script:
